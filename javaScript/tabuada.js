88aca5c1024ecd502d8c8454b8c256fdd636e539
Isabela Matos <isabelamsilvasilva@gmail.com>
2023-03-09 19:17:59 -0300

    meu primeiro commit

--- javaScript/tabuada.js
+++ javaScript/tabuada.js
@@ -1,14 +1,15 @@
 var tabuada = 9
 var porQtd = 0
+
 var contador = 11
 
+//alterei
 
-while (contador >0 ) {
+
+while (contador > 0 ) {
     res = tabuada * porQtd
     console.log(`${tabuada} X ${porQtd} = ${res}`)
     contador= contador - 1
     porQtd = porQtd + 1
-
-    
 }
 console.log('sai')
