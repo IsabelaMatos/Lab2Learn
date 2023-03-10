71ce86dd51b655c06480bf26db9c38c2429cea62
Isabela Matos <isabelamsilvasilva@gmail.com>
2023-03-10 16:02:46 -0300

    hhhh

--- javaScript/00-sistemaDeCalculadora/01-code/script.js
+++ javaScript/00-sistemaDeCalculadora/01-code/script.js
@@ -1,1 +1,14 @@
-JSON
+
+function login() {
+    const senhaCorreta = 'senha@123'
+    var senha = window.document.getElementById("txtSenha").value
+
+    if (senhaCorreta == senha) {
+        alert("Senha correta")  
+        
+    } else {
+        alert("Senha incorreta")
+    }
+    
+    
+}
