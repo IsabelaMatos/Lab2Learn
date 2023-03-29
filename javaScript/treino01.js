8ff41e82c8b5bbc4b7d46d553c5005f78fdb9321
Isabela Matos <isabelamsilvasilva@gmail.com>
2023-03-29 15:10:17 -0300

    uuuuu

---
+++ javaScript/treino01.js
@@ -0,0 +1,26 @@
+let numero = 1
+var txt =[
+    "claudisneiajuniar",
+    "Ludovico",
+    "Jubscreudo",
+    "Juselino",
+    "Mangeraldo"]
+
+var qtdLetra = txt.length
+
+while ( qtdLetra > 0) {
+    console.log(txt[qtdLetra-1])
+
+    var n1 = 10
+    var n2 = 2
+    var res = n1%n2
+    console.log(res)
+    //Fazer um programa para listar os números primos(Assistir aula do guanabara)
+    qtdLetra = qtdLetra-1
+}
+/*
+while(numero<=10) {
+   console.log(`o número agora é ${numero}`)
+    numero = numero+1
+}
+*/
